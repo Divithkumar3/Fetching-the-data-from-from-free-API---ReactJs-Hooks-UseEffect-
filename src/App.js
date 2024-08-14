74fd40191f9acc21f660b7c9b8f19a7576a4f503
Divithkumar3 <dividivithkumar@gmail.com>
2024-08-14 14:55:17 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-class ReactState extends ReactState.component{
-    constructor(){
-        super();
-            this.state={MyWebsite:"Welcome to my Website"}
-        }
-        render(){
-        return <h1>{this.state.MyWebsite}</h1>
-        }
-}
-ReactDOM.render(<ReactState/>,document.getElementById,("root"));
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// class ReactState extends ReactState.component{
+//     constructor(){
+//         super();
+//             this.state={MyWebsite:"Welcome to my Website"}
+//         }
+//         render(){
+//         return <h1>{this.state.MyWebsite}</h1>
+//         }
+// }
+// ReactDOM.render(<ReactState/>,document.getElementById,("root"));
 
-export default ReactState;
+// export default ReactState;
+
+import React from 'react';
+
+function abc() {
+    return (
+        <div className='header'>
+        <a href="" class="menu">   <div>Home</div></a> 
+        <a href="" class="menu">   <div>Contact Us</div></a> 
+        <a href="" class="menu">   <div>About us</div></a>
+        <a href="" class="menu">   <div>Sign Up</div></a>
+        <a href="" class="menu">   <div>Log in</div></a> 
+        </div>
+    );
+}
+
+export default abc;
+
 
